9c802e7bd60f0c7033b23fdc669aede99b50a69e
methom21 <mikey.thompson8@gmail.com>
2021-07-02 09:56:08 -0700

    fixed most of the error messages

--- schema.sql
+++ schema.sql
@@ -1,30 +1,31 @@
-CREATE employee_db;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
 USE employee_db;
 
 CREATE TABLE department(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    NAME VARCHAR(30) NOT NULL
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role(
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
+    department_id INT,
     INDEX dep_ind (department_id),
   CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 
 );
 
 CREATE TABLE employee(
-    id INT PRIMARY KEY AUTO_INCREMENT
+    id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
+    role_id INT,
     manager_id INT,
-      manager_id INT UNSIGNED,
+      
   INDEX man_ind (manager_id),
-  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+  CONSTRAINT fk_manager FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
 
-)
+);
--- seed.sql
+++ seed.sql
@@ -1,4 +1,5 @@
 USE employee_db;
-INSERT INTO department(name)VALUES("sales"),("finance"),("management")
-INSERT INTO role(title,salary,department_id)VALUES("salesperson", 50000, 1)
-INSERT INTO employee(first_name,last_name,role_id, manager_id)VALUES("michael","thompson",2,3)
+SET FOREIGN_KEY_CHECKS=0;
+INSERT INTO department(name)VALUES("sales"),("finance"),("management");
+INSERT INTO role(title,salary,department_id) VALUES("salesperson", 50000, 1);
+INSERT INTO employee(first_name,last_name,role_id, manager_id) VALUES("michael","thompson",2,3);
